3f18f0a676708d5e5cf223a3c9242ce6379b1e5a
peterlanier <peterlanier@me.com>
2021-04-16 23:20:32 -0700

    Query data and print on page

--- src/Planets.js
+++ src/Planets.js
@@ -1,9 +1,29 @@
+import { useQuery } from "react-query";
+import Planet from "./Planet";
+
+const fetchPlanets = async () => {
+  const res = await fetch("http://swapi.dev/api/planets/");
+  return res.json();
+};
+
 const Planets = () => {
-    return ( 
+  const { data, status } = useQuery("planets", fetchPlanets);
+  console.log(data);
+
+  return (
+    <div>
+      <h2>Planets</h2>
+      {status == "error" && <div>Error fetching Data</div>}
+      {status == "loading" && <div>Loading data ...</div>}
+      {status == "success" && (
         <div>
-        <h2>Planets</h2>
+          {data.results.map((planet) => (
+            <Planet key={planet.name} planet={planet} />
+      ))}
         </div>
-     );
-}
- 
+      )}
+    </div>
+  );
+};
+
 export default Planets;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
